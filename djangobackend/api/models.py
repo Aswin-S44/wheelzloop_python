5ba444a1159a8b003e4373f46575f92a62075230
Aswin-S-T <aswinswebart15@gmail.com>
2025-07-14 23:00:08 +0530

    basic backedn created

--- djangobackend/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class TestUser(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    age = models.IntegerField()
-
-    def __str__(self):
-        return self.name
